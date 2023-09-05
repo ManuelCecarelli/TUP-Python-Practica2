15f1582d26614d564344c5e331615d97c62ca0a9
Manuel Cecarelli <manuelcecarelli@gmail.com>
2023-09-05 00:05:56 -0300

    Resolución del ejercicio N°2

--- exercises/exercise2.py
+++ exercises/exercise2.py
@@ -1,7 +1,6 @@
 """Comparaciones Encadenadas, Cantidad Arbitraria de Parámetros, Recursividad.
 """
 
-
 def maximo_encadenado(a: float, b: float, c: float) -> float:
     """Toma 3 números y devuelve el máximo.
 
@@ -13,7 +12,11 @@ def maximo_encadenado(a: float, b: float, c: float) -> float:
 
     Referencia: https://docs.python.org/3/reference/expressions.html#comparisons # noqa: E501
     """
-
+    if a < b > c:
+        return b
+    if c < a > b:
+        return a
+    return c
 
 # NO MODIFICAR - INICIO
 assert maximo_encadenado(1, 10, 5) == 10
@@ -29,15 +32,13 @@ assert maximo_encadenado(24, 18, 9) == 24
 assert maximo_encadenado(24, 18, 18) == 24
 # NO MODIFICAR - FIN
 
-
 ###############################################################################
-
 
 def maximo_cuadruple(a: float, b: float, c: float, d: float) -> float:
     """Re-escribir para que tome 4 parámetros, utilizar la función max.
 
     Referencia: https://docs.python.org/3/library/functions.html#max"""
-
+    return max(a, b, c, d)
 
 # NO MODIFICAR - INICIO
 assert maximo_cuadruple(1, 10, 5, -5) == 10
@@ -46,15 +47,13 @@ assert maximo_cuadruple(24, 9, 18, 20) == 24
 assert maximo_cuadruple(24, 9, 18, 30) == 30
 # NO MODIFICAR - FIN
 
-
 ###############################################################################
-
 
 def maximo_arbitrario(*args) -> float:
     """Re-escribir para que tome una cantidad arbitraria de parámetros.
     Referencia: https://docs.python.org/3/tutorial/controlflow.html#arbitrary-argument-lists # noqa: E501
     """
-
+    return max(*args)
 
 # NO MODIFICAR - INICIO
 assert maximo_arbitrario(1, 10, 5, -5) == 10
